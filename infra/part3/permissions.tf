b876ce935d2906cacc3df473b687e49fa505bade
Bhuvaneshwaran Murugan <bhuvaneshwaranmurugan21@gmail.com>
2026-09-12 14:40:26 +0530

    Canonicalize Stage 5 controller policy format

--- infra/part3/permissions.tf
+++ infra/part3/permissions.tf
@@ -96,8 +96,8 @@ locals {
         Resource = ["${local.bucket_arn}/publications/*", "${local.bucket_arn}/runs/*/attempts/*/evidence/*"]
       },
       {
-        Sid      = "ConditionalControlMetadata", Effect = "Allow"
-        Action   = [
+        Sid    = "ConditionalControlMetadata", Effect = "Allow"
+        Action = [
           "dynamodb:GetItem",
           "dynamodb:BatchGetItem",
           "dynamodb:Query",
